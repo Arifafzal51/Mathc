e432e7ec22807983686466b288b583c25d70dc00
arif afzal <arif.afzal_cs20@gla.ac.in>
2021-12-09 22:21:03 +0530

    Initial commit

---
+++ src/JavaMath.java
@@ -0,0 +1,27 @@
+public class JavaMath {
+    public static void main(String[] args)
+    {
+        double x=44;
+        double y=4;
+
+        System.out.println("Maximum number is: "+Math.max(x,y));
+
+        System.out.println("Square root of x is: "+Math.sqrt(y));
+
+        System.out.println("Log of y is: "+Math.log(y));
+
+        System.out.println("log is: "+Math.log10(y));
+
+        System.out.println("power is: "+Math.log1p(y));
+
+        System.out.println("power is: "+Math.exp(y));
+
+        System.out.println("Power is: "+Math.expm1(y));
+
+
+
+
+
+
+    }
+}
