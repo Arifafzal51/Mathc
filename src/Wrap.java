a491ab50214a15a9c0fbcf0864dfdac4f9631cde
arif afzal <arif.afzal_cs20@gla.ac.in>
2021-12-09 22:53:45 +0530

    Wrapper class

---
+++ src/Wrap.java
@@ -0,0 +1,18 @@
+public class Wrap {
+    public static void main(String[]args)
+    {
+//        int y;
+//        String str="8000";
+//
+//       y=Integer.parseInt(str);//Object to premitive
+//
+//
+//        y++;
+//        System.out.println(y);
+        double y;
+        String str="3.14";
+        y=Double.parseDouble(str);
+
+        System.out.println(y);
+    }
+}
